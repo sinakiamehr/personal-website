1bfef4fec93d4ede84165ce4ed17729ad1e0b643
Sina Kiamehr Work <sinakiamehr7@email.com>
2025-03-31 17:08:50 -0700

    added a few extra featured projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -291,7 +291,7 @@ export default function Home() {
                   src="/Markdown_previewer_img.jpg"
                   alt="Project preview"
                   fill
-                  className="object-cover"
+                  className="object-cover max-w-full h-auto object-contain"
                 />
               </div>
               <div className="p-4">
@@ -310,6 +310,146 @@ export default function Home() {
                   {/* link to deployement */}
                   <a
                     href="https://markdown-previewer-v1-0-2.vercel.app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 border border-current rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* Projects Section */}
+            <div className="project-card border border-gray-200 dark:border-gray-700 rounded-lg overflow-hidden hover:shadow-lg transition-shadow">
+              <div className="aspect-video relative">
+                <Image
+                  src="/calculator_img.jpg"
+                  alt="Project preview"
+                  fill
+                  className="object-cover max-w-full h-auto object-contain"
+                />
+              </div>
+              <div className="p-4">
+                <h3 className="text-xl font-semibold mb-2">Calculator</h3>
+                <p className="text-gray-600 dark:text-gray-300 mb-4">Calculator application, designed to emulate a real life Calculator. User can perform basic arithmetic operations such as addition, subtraction, multiplication, and division. The Application supports whole numbers as well as decimal numbers</p>
+                <div className="flex gap-2">
+                  {/* link to Repo */}
+                  <a
+                    href="https://github.com/sinakiamehr/calculator"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-foreground text-background rounded-md hover:bg-opacity-90 transition-colors"
+                  >
+                    View Code
+                  </a>
+                  {/* link to deployement */}
+                  <a
+                    href="https://calculator-pi-one-92.vercel.app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 border border-current rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* Projects Section */}
+            <div className="project-card border border-gray-200 dark:border-gray-700 rounded-lg overflow-hidden hover:shadow-lg transition-shadow">
+              <div className="aspect-video relative">
+                <Image
+                  src="/drumMachine_img.jpg"
+                  alt="Project preview"
+                  fill
+                  className="object-cover max-w-full h-auto object-contain"
+                />
+              </div>
+              <div className="p-4">
+                <h3 className="text-xl font-semibold mb-2">Drum Machine</h3>
+                <p className="text-gray-600 dark:text-gray-300 mb-4">Drum Machine application, designed to behave as a real life Drum Machine. User can click on any displayed key or press the appropriate key on the keyboard in order to produce a sound.</p>
+                <div className="flex gap-2">
+                  {/* link to Repo */}
+                  <a
+                    href="https://github.com/sinakiamehr/Drum-Machine"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-foreground text-background rounded-md hover:bg-opacity-90 transition-colors"
+                  >
+                    View Code
+                  </a>
+                  {/* link to deployement */}
+                  <a
+                    href="https://drum-machine-livid-five.vercel.app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 border border-current rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* Projects Section */}
+            <div className="project-card border border-gray-200 dark:border-gray-700 rounded-lg overflow-hidden hover:shadow-lg transition-shadow">
+              <div className="aspect-video relative">
+                <Image
+                  src="/calculator_img.jpg"
+                  alt="Project preview"
+                  fill
+                  className="object-cover max-w-full h-auto object-contain"
+                />
+              </div>
+              <div className="p-4">
+                <h3 className="text-xl font-semibold mb-2">Calculator</h3>
+                <p className="text-gray-600 dark:text-gray-300 mb-4">Calculator application, designed to emulate a real life Calculator. User can perform basic arithmetic operations such as addition, subtraction, multiplication, and division. The Application supports whole numbers as well as decimal numbers</p>
+                <div className="flex gap-2">
+                  {/* link to Repo */}
+                  <a
+                    href="https://github.com/sinakiamehr/calculator"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-foreground text-background rounded-md hover:bg-opacity-90 transition-colors"
+                  >
+                    View Code
+                  </a>
+                  {/* link to deployement */}
+                  <a
+                    href="https://calculator-pi-one-92.vercel.app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 border border-current rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+                  >
+                    Live Demo
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* Projects Section */}
+            <div className="project-card border border-gray-200 dark:border-gray-700 rounded-lg overflow-hidden hover:shadow-lg transition-shadow">
+              <div className="aspect-video relative">
+                <Image
+                  src="/drumMachine_img.jpg"
+                  alt="Project preview"
+                  fill
+                  className="object-cover max-w-full h-auto object-contain"
+                />
+              </div>
+              <div className="p-4">
+                <h3 className="text-xl font-semibold mb-2">Choropleth Visualization of US Education Attainment</h3>
+                <p className="text-gray-600 dark:text-gray-300 mb-4">Drum Machine application, designed to behave as a real life Drum Machine. User can click on any displayed key or press the appropriate key on the keyboard in order to produce a sound.</p>
+                <div className="flex gap-2">
+                  {/* link to Repo */}
+                  <a
+                    href="https://github.com/sinakiamehr/choropleth-us-education-map"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="px-4 py-2 bg-foreground text-background rounded-md hover:bg-opacity-90 transition-colors"
+                  >
+                    View Code
+                  </a>
+                  {/* link to deployement */}
+                  <a
+                    href="https://choropleth-us-education-map.vercel.app"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="px-4 py-2 border border-current rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
